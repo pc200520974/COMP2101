136172a2539f777041d66ec9f91650f3fafbbf76
sanjana <200520974@student.georgianc.on.ca>
2023-06-30 17:30:31 -0400

    Modify sysconfig.sh as per exercise instructions

---
+++ bash/sysconfig.sh
@@ -0,0 +1,5 @@
+# TODO: Add a loop that iterates through each line of the input file, extract the necessary information, and print it.
+while IFS= read -r line; do
+  # Process each line and extract necessary information
+  # Print the extracted information
+done < "$input_file"
