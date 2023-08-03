9c951ddd20a66c35ec0eadc59bd241fe44ac5221
sanjana <200520974@student.georgianc.on.ca>
2023-08-03 16:15:20 -0400

    Update sysinfo.sh script

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -16,3 +16,31 @@ echo $ip_addresses
 # Display root filesystem status using df
 echo "Root Filesystem Status:"
 df -h /
+#!/bin/bash
+
+# Get the hostname and FQDN
+hostname=$(hostname)
+fqdn=$(hostname -f)
+
+# Get the operating system name and version
+os_name_version=$(lsb_release -ds)
+
+# Get the IP address of the default interface
+ip_address=$(ip route get 1.1.1.1 | grep -oP 'src \K\S+')
+
+# Get the root filesystem free space (human-friendly format)
+root_free_space=$(df -h / | awk 'NR==2 {print $4}')
+
+# Create an output template and use embedded variables
+output_template="
+Report for $hostname
+===============
+FQDN: $fqdn
+Operating System name and version: $os_name_version
+IP Address: $ip_address 
+Root Filesystem Free Space: $root_free_space
+===============
+"
+
+# Display the output
+echo "$output_template"
