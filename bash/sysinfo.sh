794bea3c1099077dabe3b6ac05685e44612bf45f
sanjana <200520974@student.georgianc.on.ca>
2023-08-03 16:21:10 -0400

    Further improve sysinfo.sh script

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -44,3 +44,64 @@ Root Filesystem Free Space: $root_free_space
 
 # Display the output
 echo "$output_template"
+#!/bin/bash
+
+# Check if the script is run with root privilege
+if [ "$(id -u)" -ne 0 ]; then
+    echo "Error: This script requires root privilege. Please run with sudo or as root."
+    exit 1
+fi
+
+# Function to check and print errors
+print_error() {
+    if [ $? -ne 0 ]; then
+        echo "Error: $1"
+        exit 1
+    fi
+}
+
+# Function to print section title
+print_section_title() {
+    echo "=============================="
+    echo "$1"
+    echo "=============================="
+}
+
+# Function to display system description
+display_system_description() {
+    print_section_title "System Description"
+    manufacturer=$(dmidecode -s system-manufacturer)
+    model=$(dmidecode -s system-product-name)
+    serial_number=$(dmidecode -s system-serial-number)
+    echo "Computer manufacturer: $manufacturer"
+    echo "Computer description or model: $model"
+    echo "Computer serial number: $serial_number"
+}
+
+# Function to display CPU information
+display_cpu_info() {
+    print_section_title "CPU Information"
+    cpu_info=$(lshw -class cpu)
+    print_error "Failed to get CPU information."
+    echo "$cpu_info" | awk '/product/ {print "CPU manufacturer and model:", $2, $3}'
+    echo "$cpu_info" | awk '/width/ {print "CPU architecture:", $2}'
+    echo "$cpu_info" | awk '/cores/ {print "CPU core count:", $2}'
+    max_speed=$(echo "$cpu_info" | awk '/capacity/ {print $2}')
+    echo "CPU maximum speed: $(echo "scale=2; $max_speed/1000" | bc) GHz"
+
+    caches=$(echo "$cpu_info" | grep -A 3 "cache" | grep "size" | awk '{print $2, $3}')
+    echo "Sizes of caches (L1, L2, L3): $caches"
+}
+
+# Function to display operating system information
+display_os_info() {
+    print_section_title "Operating System Information"
+    os_name=$(lsb_release -ds)
+    echo "Linux distro: ${os_name%% *}"
+    echo "Distro version: $(lsb_release -rs)"
+}
+
+# Main script
+display_system_description
+display_cpu_info
+display_os_info
